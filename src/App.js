58b74ca09c23e3f525bede444e4f142ad2698ba2
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2021-07-12 07:38:40 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+import React from 'react'
 import logo from './logo.svg';
-import './App.css';
+import Product from './screens/product'
+import { createTheme, ThemeProvider } from '@material-ui/core'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Quicksand",
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div >
+        <Product />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/screens/product.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import { Typography, makeStyles, Card, CardActionArea, CardMedia, CardContent, Grid } from '@material-ui/core'
+import '../App.css'
+import Realme from '../RealmeNarzo.jpg'
+
+const useStyles = makeStyles({
+    card: {
+        maxWidth: 350,
+    },
+    media: {
+        height: 350,
+
+    },
+
+})
+
+export default function product() {
+    const styles = useStyles()
+
+    return (
+        <div style={{ textAlign: 'center', alignItems: 'center', marginTop: 100}}>
+            <Typography variant='h3' style={{ fontWeight: 'bold' }} gutterBottom >  Best Smartphone under 10k </Typography>
+
+            <div style={{ padding: 20, overflowX: 'hidden' }} >
+
+                <Grid container justifyContent='center' spacing={3} style={{ flexGrow: 1 }}>
+                    <Grid item md={3} >
+
+                        <Card className={styles.card} >
+                            <CardActionArea >
+                                <CardMedia
+                                    className={styles.media}
+                                    image={Realme}
+                                    title='realme'
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                        across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+                    <Grid item md={3} >
+                        <Card className={styles.card} >
+                            <CardActionArea >
+                                <CardMedia
+                                    className={styles.media}
+                                    image={Realme}
+                                    title='realme'
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                        across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+                    <Grid item md={3} >
+                        <Card className={styles.card} >
+                            <CardActionArea >
+                                <CardMedia
+                                    className={styles.media}
+                                    image={Realme}
+                                    title='realme'
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                        across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+                    <Grid item md={3} >
+                        <Card className={styles.card} >
+                            <CardActionArea >
+                                <CardMedia
+                                    className={styles.media}
+                                    image={Realme}
+                                    title='realme'
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                        across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+                </Grid>
+                <Typography variant='h3' style={{ fontWeight: 'bold', margin: 40 }} gutterBottom >  Best Smartphone under 20k </Typography>
+
+                <Grid container justifyContent="center" spacing={3} style={{ flexGrow: 1 }} >
+                    <Grid item md={3} >
+                        <Card className={styles.card} >
+                            <CardActionArea >
+                                <CardMedia
+                                    className={styles.media}
+                                    image={Realme}
+                                    title='realme'
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                        across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+                    <Grid item md={3} >
+                        <Card className={styles.card} >
+                            <CardActionArea >
+                                <CardMedia
+                                    className={styles.media}
+                                    image={Realme}
+                                    title='realme'
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                        across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
+
+    )
+}
